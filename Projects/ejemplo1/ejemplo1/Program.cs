03d21342dea0e239ef565c5a0ca65eda332ab5ad
Wilson De Jesús Correa Berrio <Wcorrea@intergrupo.com>
2019-01-18 17:04:25 -0500

    le realizamos un cambio en el mensaje

---
+++ Projects/ejemplo1/ejemplo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
